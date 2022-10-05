e12c569edcb6b30bd7c0f3fd73abd56de4f9a305
RobertDeBiro <robert.maric.92@gmail.com>
2022-10-05 08:48:45 +0200

    Makefile practice

---
+++ Makefile_practice/Message.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Message.hpp"
+
+using namespace std;
+
+void Message::printMessage()
+{
+    cout << "Hello\n";
+}
---
+++ Makefile_practice/main.cpp
@@ -0,0 +1,12 @@
+#include "Message.hpp"
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    Message m;
+    m.printMessage();
+
+    return 0;
+}
