a8b8aba6856ab21ac8b39fdfc409c1909d9f23ea
RobertDeBiro <robert.maric.92@gmail.com>
2022-10-13 12:10:34 +0200

    Adding Adder dir, update main.cpp to use adder

--- Makefile_practice/CMake/main.cpp
+++ Makefile_practice/CMake/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-// #include "adder.h"
+#include "Adder/adder.h"
 
 int main()
 {
     std::cout << "Hello CMake!" << std::endl;
-    // std::cout << "Adder result = " << add(17.3, 16.5) << std::endl;
+    std::cout << "Adder result = " << add(17.3, 16.5) << std::endl;
 
     return 0;
 }
