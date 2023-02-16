c6c4bcad26ae98191ad1511c7df5e2ab71a07e98
RobertDeBiro <robert.maric.92@gmail.com>
2023-02-16 17:30:38 +0100

    Templates finished

--- Cpp_syntax_and_practice/15.Templates/Class_Templates/Template_class.cpp
+++ Cpp_syntax_and_practice/15.Templates/Class_Templates/Template_class.cpp
@@ -18,11 +18,14 @@ public:
 
 int main()
 {
+    std::cout << "*****************************************************\n";
+
 	Pair1<int> p1(5, 8);
 	std::cout << "Pair: " << p1.first() << ' ' << p1.second() << '\n';
 
 	const Pair1<double> p2(2.3, 4.5);
 	std::cout << "Pair: " << p2.first() << ' ' << p2.second() << '\n';
 
+    std::cout << "*****************************************************\n";
 	return 0;
 }
--- Cpp_syntax_and_practice/15.Templates/Class_Templates/Template_class_mult_params.cpp
+++ Cpp_syntax_and_practice/15.Templates/Class_Templates/Template_class_mult_params.cpp
@@ -18,11 +18,14 @@ public:
  
 int main()
 {
+    std::cout << "*****************************************************\n";
+
 	Pair<int, double> p1(5, 6.7);
 	std::cout << "Pair: " << p1.first() << ' ' << p1.second() << '\n';
 
 	const Pair<double, int> p2(2.3, 4);
 	std::cout << "Pair: " << p2.first() << ' ' << p2.second() << '\n';
 
+    std::cout << "*****************************************************\n";
 	return 0;
 }
