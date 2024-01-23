bdbe6a6a26f8619d19ef5ab25d09eda85f829aca
RobertDeBiro <robert.maric.92@gmail.com>
2024-01-23 18:03:41 +0100

    Updated comment for Preprocessor_macros.cpp

--- Cpp_syntax_and_practice/2.Cpp_Basics-Functions_and_Files/Preprocessor_macros.cpp
+++ Cpp_syntax_and_practice/2.Cpp_Basics-Functions_and_Files/Preprocessor_macros.cpp
@@ -4,8 +4,7 @@
 int main(int argc, char* argv[]) {
     std::cout << "*****************************************************\n";
 
-    //  Compile with g++ -std=c++11 or g++ -std=c++14 to
-    //  have different output.
+    //  Compile with 'g++ -std=c++11' or 'g++ -std=c++14' to have different output.
     //  more info:
     //   - https://renenyffenegger.ch/notes/development/languages/C-C-plus-plus/preprocessor/macros/predefined/__cplusplus
     //   - https://stackoverflow.com/a/7132549/180275
