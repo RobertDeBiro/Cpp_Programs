0850cf8640c3bcfb673278fdc82942951449519d
RobertDeBiro <robert.maric.92@gmail.com>
2024-05-20 09:33:16 +0200

    JSON example updated

--- JSON/src/Parse_and_update_json.cpp
+++ JSON/src/Parse_and_update_json.cpp
@@ -1,46 +1,44 @@
 /*
  * Saving data from .json file into object inside C++ program
- * - object must be of type Json::Value
- * - data is saved inside json object using another object of type Json::Reader
- * 
- * References:
- *  - https://www.youtube.com/watch?v=GYauneigGTs&t=4s
- *  - https://www.youtube.com/watch?v=2P4wdz1li5c&t=161s
- *  - https://github.com/open-source-parsers/jsoncpp
+ * - object must be of type 'Json::Value'
+ * - data is saved inside json object using another object of type 'Json::Reader'
  */
 
 #include <iostream>
 #include <string>
 #include "../inc/json.hpp"
-#include <fstream> // std::ifstream
+#include <fstream>
 
 namespace Files
 {
-    const char* students{ "C:\\Users\\Robert\\Desktop\\IT\\Programs\\Random_files\\json\\students.json" };
+    //* Windows paths
+    // const char* filename{ "C:\\Users\\Robert\\Desktop\\IT\\Programs\\Random_files\\json\\students.json" };
+
+    //* Linux paths
+    const char* filename{ "/home/robert/IT/Files_for_testing/Students" };
 }
 
 int main()
 {
     std::cout << "*****************************************************\n";
 
-    // Using fstream to get the file pointer in "file"
-    std::ifstream file{ Files::students };
-    if(!file)
+    // Using 'fstream' to get the file pointer in "file"
+    std::ifstream input_file{ Files::filename };
+    if(!input_file)
     {
-        std::cerr << "File could not be opened for reading!\n";
+        std::cerr << Files::filename << " file could not be opened for reading!" << std::endl;
         return 1;
     }
 
     Json::Value jsonData; 
     Json::Reader jsonReader;
     
-    // Parsing json by using jsonReader
-    // - parse() function transfers data from the file object (details.json file)
-    //   into Json::Value jsonData object
-    jsonReader.parse(file, jsonData);
+    // Parsing json by using 'jsonReader'
+    // - 'parse()' transfers data from the 'std::ifstream input_file' into 'Json::Value jsonData'
+    jsonReader.parse(input_file, jsonData);
     std::cout << "Total json data:\n" << jsonData << "\n";
 
-    // Print jsonData parameters individually
+    // Print 'jsonData' parameters individually
     std::cout << "Name: " << jsonData["Name"] << '\n';
     std::cout << "Birth: " << jsonData["Birth"] << '\n';
     std::cout << "College: " << jsonData["College"] << '\n';
@@ -54,9 +52,9 @@ int main()
     std::cout << "Json::Value object: " << jsonDataPractice["City"] << '\n';
     
     // Following doesn't work!
-    //  - it doesn't work because index of Json::Value must be typed as string
-    //jsonDataPractice[1] = 1;
-    //std::cout << "1. Json::Value object: " << jsonDataPractice[1] << '\n';
+    //  - it doesn't work because index of 'Json::Value' must be typed as string
+    //!jsonDataPractice[1] = 1;
+    //!std::cout << "1. Json::Value object: " << jsonDataPractice[1] << '\n';
 
     std::cout << "----------------------------------------------\n";
 
